0888cc91c2ffa3a34d0486a4cc599d673fbc9ecc
Yi Hao Wong <95235171+YHWong20@users.noreply.github.com>
2022-03-16 21:36:27 +0800

    Add files via upload

--- cool_bot.py
+++ cool_bot.py
@@ -1,5 +1,4 @@
 import os
-
 import discord
 import random
 from discord.ext import commands
@@ -8,15 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Gives bot intent permissions
 intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
-bot = commands.Bot(command_prefix= "$", description= "your mom", intents=intents)
+bot = commands.Bot(command_prefix= "$", description= "A Cool Bot", intents=intents)
 
 
 @bot.event
 async def on_ready():
-    print(f"Bot {bot.user} is online")
+    print(f"{bot.user} is online")
 
     member_dict = {}
     for guild in bot.guilds:
@@ -35,29 +35,53 @@ async def on_ready():
 
 
 @bot.event
-# Checks if user is playing BF1
+# Checks if member is playing a specified game
 async def on_member_update(before, after):
-    game = "battlefield 1"
-    channel = client.get_channel(id= 276664033971535873)
+    game = ["battlefield 1", "visual studio code"]
+    channel = bot.get_channel(id= 276664033971535873)
 
-    if after.activity and after.activity.name.lower() == game:
-        response = f"Stop Playing BF1 {after.mention}"
-        await channel.send(response)  
+    if after.activity and after.activity.name.lower() == game[0]:
+        bf_response = f"{after.mention} Playing BF1?\n https://c.tenor.com/W5fgIZhzwiAAAAAM/eyebrow-dwayne-johnson.gif"
+        await channel.send(bf_response)  
+
+    # elif after.activity and after.activity.name.lower() == game[1]:
+    #     vs_response = f"Visual studio code deez nuts {after.mention}"
+    #     await channel.send(vs_response)
     
 
-@bot.command(name= "hello")
-async def reply(ctx):
-    answers = ["hi!", "hello!", "good day!"]
+@bot.event
+# Foul Language Checker
+async def on_message(message):
+    bad_words = ["fuck", "fk", "fck", "fug", "fugg", "fark", "faggot", "fag", "fker", "fucker",\
+         "cunt", "twat", "nigger", "penis", "asshole", "bitch", "dickhead", "pussy", "vagina", "motherfucker",\
+             "motherfker", "motherfcker", "cb", "cheebai", "cheebye", "coon", "lanjiao", "lanpa", "jibai", "kns",\
+                 "lj", "fkface", "fuckface"]
+    if message.author == bot.user:
+        return    
+    if message.content in bad_words:
+        await message.channel.send(f"{message.author.mention} Watch your language!")
+    
+
+@bot.command(name= "hello", help= "Gets a reply to 'hello'")
+async def hello(ctx):
+    answers = ["hi!", "hello!", "good day!", "how are you?", ":)"]
     answer = random.choice(answers)
     await ctx.send(answer)
 
-##@bot.event
-##async def on_message(message):
-##    if message.author == client.user:
-##        return
-##            
-##    if message.author.id == 287532310130196480:
-##        await message.channel.send("ok junhan thanks 4 ur input")
-##    elif message.author.id == 286487459431186432:
-##        await message.channel.send("ok yuting thanks 4 ur input")
+
+@bot.command(name= "yuting", help= "Gets Yu Ting quote")
+async def yuting(ctx):
+    answers = ["bruh", "fk me in my ass", "bro my team fking sucks", "poggers", "i like men"]
+    answer = random.choice(answers)
+    await ctx.send(answer)
+
+
+@bot.command(name= "euhian", help= "Gets Eu Hian quote")
+async def euhian(ctx):
+    answers = ["brooo?", "yooo?", "who up meatsmith", "the egg is tamago", "bro?"]
+    answer = random.choice(answers)
+    await ctx.send(answer)
+
+
+
 bot.run(TOKEN)
